e4b9105cd48206bf6eead4a93a1afff20bd166ee
kalypa <80308782+kalypa@users.noreply.github.com>
2022-12-24 22:08:45 +0900

    씬 위치 수정

--- apiframeworktest/Scene_01.cpp
+++ apiframeworktest/Scene_01.cpp
@@ -33,18 +33,22 @@ void Scene_01::Enter()
 	SoundMgr::GetInst()->LoadSound(L"BGM1", true, L"Sound\\Scene1BGM.wav");  
 	SoundMgr::GetInst()->Stop(SOUND_CHANNEL::SC_BGM);
 	SoundMgr::GetInst()->Play(L"BGM1");
+
 	tObj = new Panel(L"StartButton", ResMgr::GetInst()->ImgLoad(L"StartButton", L"Image\\TechnologicPanel.bmp"));
-	tObj->SetPos(Vec2(380.f, 300.f));
 	tObj->SetScale(Vec2(3.f, 3.f));
+	tObj->SetPos(Vec2(Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2));
 	AddObject(tObj, GROUP_TYPE::BUTTON);
+	
 	aObj = new Arrow(L"LeftArrow", ResMgr::GetInst()->ImgLoad(L"LeftArrow", L"Image\\LeftArrow.bmp"));
-	aObj->SetPos(Vec2(50.f, 335.f));
+	aObj->SetPos(Vec2(100.f, 385.f));
 	aObj->SetScale(Vec2(3.f, 3.f));
 	AddObject(aObj, GROUP_TYPE::BUTTON);
+	
 	bObj = new Arrow(L"RightArrow", ResMgr::GetInst()->ImgLoad(L"RightArrow", L"Image\\RightArrow.bmp"));
-	bObj->SetPos(Vec2(1165.f, 335.f));
+	bObj->SetPos(Vec2(1165.f, 385.f));
 	bObj->SetScale(Vec2(3.f, 3.f));
 	AddObject(bObj, GROUP_TYPE::BUTTON);
+	
 	tPlayerTest = new Panel(L"StartButton", ResMgr::GetInst()->ImgLoad(L"Button", L"Image\\TechnologicPanel.bmp"));
 	tPlayerTest->SetPos(Vec2(10.f, 10.f));
 	tPlayerTest->SetScale(Vec2(0.5f, 0.5f));
